58efb0dd91f54b7a2e9c87e81059900e8ce87983
Robert Dayton <robertcdayton@gmail.com>
2017-02-01 15:57:08 -0500

    More tests

--- functional_tests/test_search_validation.py
+++ functional_tests/test_search_validation.py
@@ -6,15 +6,23 @@ class SearchValidationTest(FunctionalTest):
     def test_cannot_add_empty_list_items(self):
     # Edith goes to the home page and accidentally tries to submit
     # an empty search. She hits Enter on the empty input box
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_id('name_input').send_keys('\n')
 
     # The home page refreshes, and there is an error message saying
     # that list items cannot be blank
-
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "Search field can not be blank")
     # She tries again with some text for the item, which now works
-
+        self.browser.find_element_by_id('name_input').send_keys('Bob')
+        self.check_for_row_in_table('Bob')
     # Perversely, she now decides to submit a second blank list item
+        self.browser.find_element_by_id('name_input').send_keys('\n')
 
     # She receives a similar warning on the list page
-
+        self.browser.find_element_by_id('name_input').send_keys('Bob')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "Search field can not be blank")
     # And she can correct it by filling some text in
-        self.fail('write me!')
+        self.browser.find_element_by_id('id_new_item').send_keys('Bob')
+        self.check_for_row_in_table('Bob')
--- functional_tests/test_simple_home_page_view.py
+++ functional_tests/test_simple_home_page_view.py
@@ -7,4 +7,30 @@ class NewVisitorTest(FunctionalTest):
         self.browser.get(self.server_url)
 
         self.assertIn('newtracker', self.browser.title)
+        
+        # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('name_input')
+        self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                'Search'
+        )
+
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
+        inputbox.send_keys('Bob')
+
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list table
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == 'Bob' for row in rows)
+        )
+
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very
+        # methodical)
+        self.fail('Finish the test!')
 
