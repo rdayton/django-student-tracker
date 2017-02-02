a0d8f610492e2aa749c1eeb22f57b7d934838893
bobsnewgit <bobsnewgit@gmail.com>
2017-02-02 06:51:52 -0500

    home updated

--- newtracker/views.py
+++ newtracker/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from newtracker.users.models import User
 
 # Create your views here.
 def home_page(request):
+    if request.method == 'POST':
+        new_name_input = request.POST['name_input']
+        User.objects.create_user(new_name_input)
+        return redirect('/')
+
+
     return render(request, 'pages\home.html',{
-        'new_item_text': request.POST.get('item_text',''),
+        'new_name_input': request.POST.get('name_input',''),
     })
