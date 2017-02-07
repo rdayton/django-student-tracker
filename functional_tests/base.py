6860b0853824cb7b415312d6b908231c3ebaec14
bobsnewgit <bobsnewgit@gmail.com>
2017-02-06 21:28:32 -0500

    screencaps on test fail

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -8,9 +8,13 @@ from newtracker.users.models import Student
 from django.conf import settings
 from selenium.common.exceptions import WebDriverException
 import time
+import os
+from datetime import datetime
 
 MAX_WAIT = 5
-
+SCREEN_DUMP_LOCATION = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'screendumps'
+)
 class FunctionalTest(StaticLiveServerTestCase):
     #fixtures = ['initial.json']
     #replace fixtures with model_mommy
@@ -68,7 +72,38 @@ class FunctionalTest(StaticLiveServerTestCase):
        # self.student.save()
 
     def tearDown(self):
+        if self._test_has_failed():
+            if not os.path.exists(SCREEN_DUMP_LOCATION):
+                os.makedirs(SCREEN_DUMP_LOCATION)
+            for ix, handle in enumerate(self.browser.window_handles):
+                self._windowid = ix
+                self.browser.switch_to_window(handle)
+                self.take_screenshot()
+                self.dump_html()
         self.browser.quit()
         return super().tearDown()
 
-    
+    def _test_has_failed(self):
+        # slightly obscure but couldn't find a better way!
+        return any(error for (method, error) in self._outcome.errors)
+
+    def take_screenshot(self):
+        filename = self._get_filename() + '.png'
+        print('screenshotting to', filename)
+        self.browser.get_screenshot_as_file(filename)
+
+    def dump_html(self):
+        filename = self._get_filename() + '.html'
+        print('dumping page HTML to', filename)
+        with open(filename, 'w') as f:
+            f.write(self.browser.page_source)
+
+    def _get_filename(self):
+        timestamp = datetime.now().isoformat().replace(':', '.')[:19]
+        return '{folder}/{classname}.{method}-window{windowid}-{timestamp}'.format(
+            folder=SCREEN_DUMP_LOCATION,
+            classname=self.__class__.__name__,
+            method=self._testMethodName,
+            windowid=self._windowid,
+            timestamp=timestamp
+        )
