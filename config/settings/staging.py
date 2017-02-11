3ffb3155225d4e0199a250835d77ec5ee192cb5a
bobsnewgit <bobsnewgit@gmail.com>
2017-02-11 07:20:00 -0500

    root dir in staging config

--- config/settings/staging.py
+++ config/settings/staging.py
@@ -168,7 +168,7 @@ LOGGING = {
         'logfile': {
             'level':'DEBUG',
             'class':'logging.FileHandler',
-            'filename': BASE_DIR + "/../logfile",
+            'filename': ROOT_DIR + "/../logfile",
         },
     },
     'root': {
