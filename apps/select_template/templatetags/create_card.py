f18a5cc85151623977d1a5973a6aa31bbb3b7645
Robert Dayton <robertcdayton@gmail.com>
2017-03-06 13:32:36 -0500

    color added as option to card tag

--- apps/select_template/templatetags/create_card.py
+++ apps/select_template/templatetags/create_card.py
@@ -3,8 +3,8 @@ from django import template
 register = template.Library()
 
 @register.inclusion_tag('create_card.html')
-def create_card( id, template_name ,title):
-    return { 'id': id, 'template_name':template_name,'title':title}
+def create_card( id, template_name ,title, color):
+    return { 'id': id, 'template_name':template_name,'title':title, 'color':color}
 
 @register.inclusion_tag('create_multi_card.html')
 def create_multi_card(  template_name, ids,title):
