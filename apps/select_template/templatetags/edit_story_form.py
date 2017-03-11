039dc2de3eddff4fc1e419be58693e65c2b78564
bobsnewgit <bobsnewgit@gmail.com>
2017-03-11 10:34:20 -0500

    show modal button will be added as template tag

--- apps/select_template/templatetags/edit_story_form.py
+++ apps/select_template/templatetags/edit_story_form.py
@@ -6,3 +6,6 @@ register = template.Library()
 def show_all_from_field( student, form):
     return { 'student':student, 'form':form }
 
+@register.inclusion_tag('story_modal_button.html')
+def show_modal_button( pk):
+    return { 'pk':pk}
